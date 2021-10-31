383de353dea6290e479691cd458050eee4965cfd
MikaBarDEV <mikabar002DEV@gmail.com>
2021-10-31 08:32:30 +0000

    Added [bugged] tag to ignore when searched

--- guide/.vuepress/parseTags.js
+++ guide/.vuepress/parseTags.js
@@ -5,9 +5,8 @@ module.exports= (page)=>{
     const $ = cheerio.load(page.contentRendered);
     $('Badge').each(function (i,elem){
         let txt = $(this).attr("text");
-        if(txt.match(/Easy|Difficult|Read Below|Medium/gi)) return;
+        if(txt.match(/Easy|Difficult|Read Below|Medium|Bugged/gi)) return;
         if(txt.length>1) tags.push(txt);
     })
     return tags;
 }
-
